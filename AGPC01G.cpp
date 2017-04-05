ebc4ac50c7607a2da7d5acb0648fdf0046936806
Mahmud Allahverdiyev <allahverdiyev.062@gmail.com>
2017-04-05 21:52:47 +0400

    Add files via upload

---
+++ AGPC01G.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+//#include <unordered_map>
+#include <list>
+#include <bitset>
+#include <utility>
+#include <cassert>
+#include <iomanip>
+#include <ctime>
+#include <fstream>
+#include <bitset>
+
+using namespace std;
+
+const int me = 100025;
+const int mod = 1000000007;
+
+int T, N;
+int a[me];
+
+int main(int argc, const char * argv[]) {
+    //ios_base::sync_with_stdio(0);
+    //cin.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    
+    // found using http://oeis.org/search?q=1%2C+2%2C+4%2C+10&language=english&go=Search
+    
+    a[0] = 1, a[1] = 1;
+    for(int i = 2; i < me; i ++)
+        a[i] = (a[i - 1] + 1LL * (i - 1) * a[i - 2] % mod) % mod;
+    scanf("%d", &T);
+    while(T --){
+        scanf("%d", &N);
+        printf("%d\n", a[N]);
+    }
+
+    return 0;
+}
