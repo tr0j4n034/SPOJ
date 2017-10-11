8ee2b8d968f9e6ea2d04354697d9a96fef8729ba
Mahmud Allahverdiyev <allahverdiyev.062@gmail.com>
2017-10-11 18:11:21 +0000

    See sharp phi

---
+++ ETF.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Practice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const int MAX = 1000005;
+            int[] primes = new int[MAX];
+            int[] lp = new int[MAX];
+            int[] phi = new int[MAX];
+
+            for (int i = 2; i < MAX; i ++) {
+                if (primes[i] > 0) {
+                    continue;
+                }
+                for (int j = i; j < MAX; j += i) {
+                    primes[j] = 1;
+                    lp[j] = i;
+                }
+            }
+            phi[1] = 1;
+            for (int i = 2; i < MAX; i ++) {
+                int j = i;
+                while (j % lp[i] == 0) {
+                    j /= lp[i];
+                }
+                phi[i] = phi[j] * (lp[i] - 1) * i / j / lp[i];
+            }
+
+            int T, N;
+            T = Convert.ToInt32(Console.ReadLine());
+            while (T -- > 0) {
+                N = Convert.ToInt32((Console.ReadLine()));
+                Console.WriteLine(phi[N]);
+            }
+        }
+    }
+}
