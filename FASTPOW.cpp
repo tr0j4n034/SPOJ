e93a06c61d5d8e291ec3cb0b3945133b1def7023
Mahmud Allahverdiyev <tr0j4n034@gmail.com>
2017-11-04 23:37:49 +0400

    as we always did

---
+++ FASTPOW.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  practice
+//
+//  Created by Mahmud on 11/04/17.
+//  Copyright © 2017 Mahmud. All rights reserved.
+//
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const int MAX = 10005;
+
+int a, b, c;
+
+int power(int a, int b, int modulo) {
+    if (b == 0) {
+        return 1 % modulo;
+    }
+    if (b & 1) {
+        return 1LL * power(a, b - 1, modulo) * a % modulo;
+    }
+    int half = power(a, b >> 1, modulo);
+    return 1LL * half * half % modulo;
+}
+
+int main() {
+    cin >> a >> b >> c;
+    cout << power(a, b, c) << endl;
+    
+    return 0;
+}
